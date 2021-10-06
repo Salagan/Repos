fc45309dcbfcb81ef3f9163a4b030bf2fd0cf2c3
Salagan <andriysalagan@gmail.com>
2021-10-06 16:45:00 +0300

    Changes to ZRdemo2 (no important)

---
+++ ZRdemo2/ZRdemo2/Models/Gym.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZRdemo2.Models
+{
+    public class Gym
+    {
+        public string Address { get; set; }
+    }
+}
